b922435d334e187edb42310963693b79a8b01ec9
arthyrych <arthyrych@gmail.com>
2023-01-30 10:21:02 +0200

    added a stub for test

---
+++ specs/test.spec.ts
@@ -0,0 +1,17 @@
+import {agent as request} from 'supertest'
+import {expect} from 'chai'
+import env from '../config'
+
+describe('test suite', () => {
+  before(async () => {
+    // add before hook
+  })
+
+  it('test case', async () => {
+    // add test case
+  })
+
+  after(async () => {
+    // add after hook
+  })
+})
